31613755da13202482585bb662396ae322f085cb
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2016-11-15 17:22:30 +0100

    Fixing dom selector - removing unnecesery function

--- 01.DOM_selector/src/selector.js
+++ 01.DOM_selector/src/selector.js
@@ -1,16 +1,12 @@
 var domSelector = function(selectors) {
   'use strict';
 
-  function getHtmlSelector(selectors) {
-
-    try {
-      var selectedElement = document.querySelectorAll(selectors);
-      var selectedElementArray = Array.prototype.slice.call(selectedElement);
-      return selectedElementArray;
-    } catch (e) {
-      throw new Error('Invalid or non existing selector');
-    }
+  try {
+    var selectedElement = document.querySelectorAll(selectors);
+    var selectedElementArray = Array.prototype.slice.call(selectedElement);
+    return selectedElementArray;
+  } catch (e) {
+    throw new Error('Invalid or non existing selector');
   }
 
-  return getHtmlSelector(selectors)
 };
