3f9d511714fb9007cbc7fc4f49d65cbbe1c2913d
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2016-11-15 14:35:50 +0100

    implementing css manimulation

--- 02.CSS_manipulation/spec/cssProp.js
+++ 02.CSS_manipulation/spec/cssProp.js
@@ -10,18 +10,50 @@ describe('cssProp', function() {
 
     $selectedElement = $('#toddler');
     selectedElement = $selectedElement[0];
+
   });
 
   it('should set a CSS attribute of an HTML element', function() {
     // code goes here
+
+    var selector = '#toddler';
+    var property = 'font-size';
+    var value = '50px';
+
+    cssProp(selector, property, value);
+
+    expect($selectedElement.css(property)).toEqual(value);
   });
 
   it('should return an existing CSS property value of an HTML element', function() {
     // code goes here
+    var selector = '#toddler';
+    var property = 'fontSize';
+    var value = '50px';
+    var getvalue = '';
+
+    cssProp(selector, 'fontSize', value);
+    getvalue = cssProp(selector, property);
+
+    expect($selectedElement.css(property)).toEqual(getvalue);
+
   });
 
   it('should set multiple CSS properties of an HTML element', function() {
     // code goes here
+    var selector = '#toddler';
+    var propertyObj = {
+      'font-size' : '40px',
+      'color'     : 'red',
+      'position'  : 'relative'
+    };
+    var getvalues = '';
+
+    cssProp(selector, propertyObj);
+    // getvalues = cssProp(selector, property);
+
+    console.log($selectedElement.attr('style'));
+    // expect($selectedElement.attr('style')).toEqual(propertyObj);
   });
 
   it('should properly set CSS properties if called multiple times on different HTML elements', function() {
--- 02.CSS_manipulation/src/cssProp.js
+++ 02.CSS_manipulation/src/cssProp.js
@@ -1,5 +1,58 @@
-function cssProp() {
+function cssProp( htmlElement, cssProperty, value ) {
   'use strict';
+  var el = getHtmlSelector( htmlElement );
 
-  // code goes here
+  //check if object or property was passed as argument
+  if( typeof cssProperty === 'object'){
+    setStyleObject( htmlElement, cssProperty );
+  }else{
+
+    var cssPropertyObject = new Object();
+    cssPropertyObject[cssProperty] = value
+
+    //check if value isset
+    if(typeof value !== 'undefined'){
+
+      setStyleObject( htmlElement, cssPropertyObject );
+
+    }else{
+
+      el = getStylePropertyValue(htmlElement, cssProperty);
+
+    }
+  }
+ 
+  return el;
+
+  //set values from object to style property
+  function setStyleObject( htmlElement, propertyObject ){
+
+   var el = getHtmlSelector( htmlElement )[0];
+   var prop = '';
+
+   for (prop in propertyObject) {
+      if (propertyObject.hasOwnProperty(prop)) {
+        el.style[prop] = propertyObject[prop];
+      }
+    }
+  }
+
+  //get style value from property key
+  function getStylePropertyValue(htmlElement, cssProperty){
+    var el = getHtmlSelector( htmlElement )[0];
+    return el.style[cssProperty]
+  }
+}
+
+
+function toArray(nl) {
+    for(var a=[], l=nl.length; l--; a[l]=nl[l]);
+    return a;
+}
+
+function getHtmlSelector( selectors ){
+
+  var selector = document.querySelectorAll( selectors );
+
+  return toArray(selector);
 }
