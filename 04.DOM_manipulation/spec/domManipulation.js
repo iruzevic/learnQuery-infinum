a361c47070e1ad898e91eb62f6d81c800b220af6
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2016-11-16 17:06:21 +0100

    DOM manipulation, prepend, remove and append

--- 04.DOM_manipulation/spec/domManipulation.js
+++ 04.DOM_manipulation/spec/domManipulation.js
@@ -1,25 +1,44 @@
 describe('domManipulation', function() {
   'use strict';
 
-  var $selectedElement, selectedElement;
+  var $selectedElement, selectedElement, domSelector;
 
   beforeEach(function() {
     affix('.learn-query-testing #toddler .hidden.toy+h1[class="title"]+span[class="subtitle"]+span[class="subtitle"]+input[name="toyName"][value="cuddle bunny"]+input[class="creature"][value="unicorn"]+.hidden+.infinum[value="awesome cool"]');
 
     $selectedElement = $('#toddler');
     selectedElement = $selectedElement[0];
+
+    domSelector = new dom();
   });
 
   it('should be able to remove a HTML element', function() {
-    // code goes here
+
+    expect(selectedElement).not.toBeUndefined();
+
+    domSelector.remove(selectedElement);
+
+    expect($('#toddler')[0]).toBeUndefined();
   });
 
   it('should append a HTML element to the given element', function() {
-    // code goes here
+
+    expect($selectedElement.find('p')[0]).toBeUndefined();
+
+    domSelector.append(selectedElement, 'p');
+
+    expect($selectedElement.find('p')[0]).not.toBeUndefined();
+
   });
 
   it('should prepend a HTML element to the given element', function() {
     // code goes here
+
+    expect($selectedElement.find('p')[0]).toBeUndefined();
+
+    domSelector.prepend(selectedElement, 'p');
+
+    expect($selectedElement.find('p')[0]).not.toBeUndefined();
   });
 
   it('should be able to add a new HTML element after a given HTML element', function() {
--- 04.DOM_manipulation/src/domManipulation.js
+++ 04.DOM_manipulation/src/domManipulation.js
@@ -1,5 +1,27 @@
 var dom = function(){
   'use strict';
 
-  // code goes here
+  function remove(element){
+
+    element.parentNode.removeChild(element);
+
+  }
+
+  function append(targetElement, element) {
+      var newElement = document.createElement(element);
+      targetElement.appendChild(newElement);
+  }
+
+  function prepend(targetElement, element) {
+
+      var newElement = document.createElement(element);
+      targetElement.insertBefore(newElement, targetElement.firstChild);
+
+  }
+
+  return {
+    remove: remove,
+    append: append,
+    prepend: prepend
+  }
 };
