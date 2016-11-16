00f8a6cb20913d84958cfbd9cfb74084e61eb8b0
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2016-11-16 12:52:20 +0100

    Fixing task to input htmlElement not selector

--- 02.CSS_manipulation/spec/cssProp.js
+++ 02.CSS_manipulation/spec/cssProp.js
@@ -14,77 +14,47 @@ describe('cssProp', function() {
   });
 
   it('should set a CSS attribute of an HTML element', function() {
-
-    var selector = '#toddler';
     var property = 'fontSize';
     var value = '50px';
-    var expectedPropertyValue = '';
 
-    cssProp(selector, property, value);
+    cssProp(selectedElement, property, value);
 
-    expectedPropertyValue = $selectedElement.css(property);
-
-    expect(cssProp(selector, property)).toEqual(expectedPropertyValue);
+    expect($selectedElement.css(property)).toEqual(value);
   });
 
   it('should return an existing CSS property value of an HTML element', function() {
 
-    var selector = '#toddler';
-    var property = 'fontSize';
-    var value = '50px';
-    var expectedPropertyValue = '';
+    var property = 'color';
 
-    cssProp(selector, property, value);
-
-    expectedPropertyValue = $selectedElement.css(property);
-
-    expect(cssProp(selector, property)).toEqual(expectedPropertyValue);
+    expect($selectedElement.css(property)).toEqual(cssProp(selectedElement, property));
 
   });
 
   it('should set multiple CSS properties of an HTML element', function() {
 
-    var selector = '#toddler';
     var propertyObj = {
       'font-size' : '40px',
       'color'     : 'red',
       'position'  : 'relative'
     };
-    var expectedPropertyValues = '';
 
-    cssProp(selector, propertyObj);
+    cssProp(selectedElement, propertyObj);
 
-    expectedPropertyValues = $selectedElement.attr('style');
-
-    expect(cssProp(selector)).toMatch(expectedPropertyValues);
+    expect(cssProp(selectedElement)).toMatch($selectedElement.attr('style'));
   });
 
   it('should properly set CSS properties if called multiple times on different HTML elements', function() {
 
-    var property = 'fontSize';
-    var selectorValueArray = [
-      {
-        selector: '#toddler',
-        value: '50px',
-      },
-      {
-        selector: '.learn-query-testing',
-        value: '25px',
-      },
-      {
-        selector: '#toddler',
-        value: '15px'
-      }
-    ];
+    var property = 'color';
+    var $anotherSelectedElement = $('.learn-query-testing');
+    var anotherSelectedElement = $anotherSelectedElement[0];
 
-    selectorValueArray.forEach( function (arrayItem){
-
-      cssProp(arrayItem.selector, property, arrayItem.value);
-
-      expect(cssProp(arrayItem.selector, property)).toEqual($(arrayItem.selector).css(property));
-
-    });
+    cssProp(selectedElement, property, 'red');
+    cssProp(anotherSelectedElement, property, 'red');
+    cssProp(anotherSelectedElement, property, 'blue');
 
 
-  });
+    expect($selectedElement.css(property)).toEqual(cssProp(selectedElement, property));
+    expect($anotherSelectedElement.css(property)).toEqual(cssProp(anotherSelectedElement, property));
+  }); 
 });
--- 02.CSS_manipulation/src/cssProp.js
+++ 02.CSS_manipulation/src/cssProp.js
@@ -1,6 +1,6 @@
 function cssProp( htmlElement, cssProperty, value ) {
   'use strict';
-  var el = getHtmlSelector( htmlElement );
+  var el = '';
 
   //check if object or property was passed as argument
   if( typeof cssProperty === 'object') {
@@ -20,12 +20,12 @@ function cssProp( htmlElement, cssProperty, value ) {
       if(typeof cssProperty !== 'undefined') {
 
         //get property value
-        el = getStylePropertyValue(htmlElement, cssProperty);
+        el = window.getComputedStyle(htmlElement, null).getPropertyValue(cssProperty);
 
       } else {
 
         //if no property provider, return all style on element
-        el = el[0].style.cssText;
+        el = htmlElement.style.cssText;
 
       }
 
@@ -38,30 +38,12 @@ function cssProp( htmlElement, cssProperty, value ) {
   //set values from object to style property
   function setStyleObject( htmlElement, propertyObject ) {
 
-    var el = getHtmlSelector( htmlElement )[0];
     var prop = '';
 
     for (prop in propertyObject) {
        if (propertyObject.hasOwnProperty(prop)) {
-         el.style[prop] = propertyObject[prop];
+         htmlElement.style[prop] = propertyObject[prop];
        }
      }
-  }
-
-  //get style value from property key
-  function getStylePropertyValue(htmlElement, cssProperty) {
-    var el = getHtmlSelector( htmlElement )[0];
-    return el.style[cssProperty]
-  }
-}
-
-function getHtmlSelector(selectors) {
-
-  try {
-    var selectedElement = document.querySelectorAll(selectors);
-    var selectedElementArray = Array.prototype.slice.call(selectedElement);
-    return selectedElementArray;
-  } catch (e) {
-    throw new Error('Invalid or non existing selector');
   }
 }
