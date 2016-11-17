996a9cac80e9b366c6484513542253252dfb14a1
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2016-11-17 13:43:38 +0100

    Updating dom manipulation

--- 04.DOM_manipulation/spec/domManipulation.js
+++ 04.DOM_manipulation/spec/domManipulation.js
@@ -4,7 +4,7 @@ describe('domManipulation', function() {
   var $selectedElement, selectedElement, domSelector;
 
   beforeEach(function() {
-    affix('.learn-query-testing #toddler .hidden.toy+h1[class="title"]+span[class="subtitle"]+span[class="subtitle"]+input[name="toyName"][value="cuddle bunny"]+input[class="creature"][value="unicorn"]+.hidden+.infinum[value="awesome cool"]');
+    affix('.learn-query-testing #toddler .hidden.toy+h1[class="title"]+span[class="subtitle"]+span[class="subtitle"]+input[name="toyName"][value="cuddle bunny"]+input[class="creature"][value="unicorn"]+.hidden+.infinum[value="awesome cool"]+select>(option[value="awesome"]+option[value="cool"])');
 
     $selectedElement = $('#toddler');
     selectedElement = $selectedElement[0];
@@ -23,57 +23,69 @@ describe('domManipulation', function() {
 
   it('should append a HTML element to the given element', function() {
 
-    expect($selectedElement.find('p')[0]).toBeUndefined();
+    expect($selectedElement.children().last().prop('outerHTML')).not.toEqual('<p></p>');
 
-    console.log(selectedElement, $selectedElement);
+    domSelector.append(selectedElement, 'p');
 
-    // domSelector.append(selectedElement, 'p');
-    console.log(selectedElement, $selectedElement);
-
-    expect($selectedElement.find('p')[0]).not.toBeUndefined();
+    expect($selectedElement.children().last().prop('outerHTML')).toEqual('<p></p>');
 
   });
 
   it('should prepend a HTML element to the given element', function() {
 
-    expect($selectedElement.find('p')[0]).toBeUndefined();
+    expect($selectedElement.children().first().prop('outerHTML')).not.toEqual('<p></p>');
 
     domSelector.prepend(selectedElement, 'p');
 
-    expect($selectedElement.find('p')[0]).not.toBeUndefined();
+    expect($selectedElement.children().first().prop('outerHTML')).toEqual('<p></p>');
   });
 
   it('should be able to add a new HTML element after a given HTML element', function() {
 
+    expect($selectedElement.next().prop('outerHTML')).not.toEqual('<p></p>');
+
     domSelector.after(selectedElement, 'p');
+
+    expect($selectedElement.next().prop('outerHTML')).toEqual('<p></p>');
   });
 
   it('should be able to add a new HTML element before a given HTML element', function() {
 
+    expect($selectedElement.prev().prop('outerHTML')).not.toEqual('<p></p>');
+
     domSelector.before(selectedElement, 'p');
+
+    expect($selectedElement.prev().prop('outerHTML')).toEqual('<p></p>');
   });
 
   it('should return a value of a given HTML non-select element', function() {
 
-    domSelector.val(selectedElement);
+    expect(domSelector.val($selectedElement.find('input')[0])).toEqual('cuddle bunny');
+    expect(domSelector.val($selectedElement.find('h1')[0])).toBeUndefined();
+
   });
 
-  it('should return a value of a given select HTML element', function(){    // code goes here
+  it('should return a value of a given select HTML element', function(){
 
-    domSelector.val(selectedElement);
+    expect(domSelector.val($selectedElement.find('select')[0])).toEqual('awesome');
+
   });
 
   it('should not throw exception if the target element is not in the DOM when calling dom.remove', function() {
 
-    selectedElement = 'p';
+    var $nonExistingElement = $('toddlers');
+    var nonExistingElement = $nonExistingElement[0];
 
-    expect(function() {domSelector.remove(selectedElement)}).toThrowError('Invalid or non existing element');
+    expect(function() {domSelector.remove(nonExistingElement)}).toThrowError('target element not provided or not in dom');
 
   });
 
   it('should not throw exception if the target element is not in the DOM when calling dom.after', function() {
 
-    expect(function() {domSelector.after(selectedElement, 'p')}).toThrowError('Invalid or non existing element');
+    var $nonExistingElement = $('toddlers');
+    var nonExistingElement = $nonExistingElement[0];
+
+    expect(function() {domSelector.after(nonExistingElement, 'p')}).toThrowError('target element not provided or not in dom');
 
   });
 });
--- 04.DOM_manipulation/src/domManipulation.js
+++ 04.DOM_manipulation/src/domManipulation.js
@@ -3,25 +3,89 @@ var dom = function(){
 
   function remove(element){
 
+    if(!element){
+      throw new Error('target element not provided or not in dom');
+    }
+
     element.parentNode.removeChild(element);
 
   }
 
   function append(targetElement, element) {
+
+      if(!targetElement){
+        throw new Error('target element not provided or not in dom');
+      }
+
+      if(!element){
+        throw new Error('element not provided');
+      }
+
       var newElement = document.createElement(element);
       targetElement.appendChild(newElement);
   }
 
   function prepend(targetElement, element) {
 
+      if(!targetElement){
+        throw new Error('target element not provided or not in dom');
+      }
+
+      if(!element){
+        throw new Error('element not provided');
+      }
+
       var newElement = document.createElement(element);
       targetElement.insertBefore(newElement, targetElement.firstChild);
 
   }
 
+  function after(targetElement, element) {
+
+    if(!targetElement){
+      throw new Error('target element not provided or not in dom');
+    }
+
+    if(!element){
+      throw new Error('element not provided');
+    }
+
+    var newElement = document.createElement(element);
+    targetElement.parentNode.insertBefore(newElement, targetElement.nextSibling);
+
+  }
+
+  function before(targetElement, element) {
+
+    if(!targetElement){
+      throw new Error('target element not provided or not in dom');
+    }
+
+    if(!element){
+      throw new Error('element not provided');
+    }
+
+    var newElement = document.createElement(element);
+    targetElement.parentNode.insertBefore(newElement, targetElement);
+
+  }
+
+  function val(targetElement) {
+
+    if(!targetElement){
+      throw new Error('target element not provided or not in dom');
+    }
+
+    return targetElement.value;
+
+  }
+
   return {
-    remove: remove,
-    append: append,
-    prepend: prepend
+    remove:   remove,
+    append:   append,
+    prepend:  prepend,
+    after:    after,
+    before:   before,
+    val:      val
   }
 };
