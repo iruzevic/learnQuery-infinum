bca9340281a91f2dd0672a7c6b52606923e4fedf
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2016-11-23 15:35:56 +0100

    Additionl fix

--- 02.CSS_manipulation/src/cssProp.js
+++ 02.CSS_manipulation/src/cssProp.js
@@ -1,26 +1,31 @@
-function cssProp(htmlElement, cssProperty, value) {
+// set values from object to style property
+function setStyleObject(element, propertyObject) {
   'use strict';
 
-  // set values from object to style property
-  function setStyleObject(element, propertyObject) {
+  var prop = '';
 
-    var prop = '';
-
-    for (prop in propertyObject) {
-      if (propertyObject.hasOwnProperty(prop)) {
-        element.style[prop] = propertyObject[prop];
-      }
+  for (prop in propertyObject) {
+    if (propertyObject.hasOwnProperty(prop)) {
+      element.style[prop] = propertyObject[prop];
     }
   }
+}
 
-  function getPropertyValue(element, cssPropertyValue) {
-    return window.getComputedStyle(element, null).getPropertyValue(
-      cssPropertyValue);
-  }
+function getPropertyValue(element, cssPropertyValue) {
+  'use strict';
 
-  function getAllElementStyles(element) {
-    return element.style.cssText;
-  }
+  return window.getComputedStyle(element, null).getPropertyValue(
+    cssPropertyValue);
+}
+
+function getAllElementStyles(element) {
+  'use strict';
+
+  return element.style.cssText;
+}
+
+function cssProp(htmlElement, cssProperty, value) {
+  'use strict';
 
   if (!htmlElement) {
     throw new Error('HTML Element not provided');
